e966c7c220f12ab23b76ca0f173c7babc03e6695
Dmitriy Kuzmin <dmitriykuzmin@ymail.com>
2026-03-16 11:48:43 +0200

    feat: Create basic structure of views in views.py

--- app/views.py
+++ app/views.py
@@ -1,3 +1,38 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index_view(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Index page")
+
+
+class TaskListView:
+    pass
+
+
+class TaskCreateView:
+    pass
+
+
+class TaskUpdateView:
+    pass
+
+
+class TaskDeleteView:
+    pass
+
+
+class TagListView:
+    pass
+
+
+class TagCreateView:
+    pass
+
+
+class TagUpdateView:
+    pass
+
+
+class TagDeleteView:
+    pass
