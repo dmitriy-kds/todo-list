b1ce4db44ac6a35e8d72247b1ebce362d6786542
Dmitriy Kuzmin <dmitriykuzmin@ymail.com>
2026-03-16 13:47:41 +0200

    feat: Add and migrate ordering to Task model

--- app/models.py
+++ app/models.py
@@ -2,14 +2,20 @@ from django.db import models
 
 
 class Task(models.Model):
+    status_choices = {
+        True: "Done",
+        False: "Not done",
+    }
+
     content = models.TextField(max_length=500, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField(blank=True, null=True)
-    status = models.BooleanField(default=False)
+    status = models.BooleanField(choices=status_choices, default=False)
     tags = models.ManyToManyField("Tag", related_name="tasks")
 
     class Meta:
         verbose_name_plural = "tasks"
+        ordering = ["status", "-created_at"]
 
     def __str__(self):
         return f"Task: {self.content}, Status: {self.status}, Created at: {self.created_at}"
