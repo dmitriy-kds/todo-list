801aba6ba9995cab2f1c060c9d672f0fadadc823
Dmitriy Kuzmin <dmitriykuzmin@ymail.com>
2026-03-16 15:13:56 +0200

    fix: Separate task list and task create views

--- app/urls.py
+++ app/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from app.views import (
     TaskListView,
-    TaskCreateView,
     TaskUpdateView,
     TaskDeleteView,
+    TaskCreateView,
     TagListView,
     TagCreateView,
     TagUpdateView,
@@ -13,7 +13,7 @@ from app.views import (
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task_list"),
-    path("", TaskCreateView.as_view(), name="task_create"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task_create"),
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task_update"),
     path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task_delete"),
     path("tags/", TagListView.as_view(), name="tag_list"),
--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
 from django.db.models import QuerySet
 from django.http import HttpRequest, HttpResponse
+from django.urls import reverse, reverse_lazy
 from django.views import generic
 from django.shortcuts import render
 
+from app.forms import TaskCreateForm
 from app.models import Task
 
 
@@ -12,6 +14,9 @@ class TaskListView(generic.ListView):
 
 class TaskCreateView(generic.CreateView):
     model = Task
+    form_class = TaskCreateForm
+    success_url = reverse_lazy("app:task_list")
+
 
 
 class TaskUpdateView(generic.UpdateView):
